abf0d20f4e5a0e7b9fdd581c60c867819644e123
Arthur <Arthur@Arthur-PC>
2015-03-24 00:26:11 -0300

    fix #38, modificada a forma de checagem pela senha

--- webBudget/src/main/java/br/com/webbudget/domain/service/AccountService.java
+++ webBudget/src/main/java/br/com/webbudget/domain/service/AccountService.java
@@ -105,6 +105,11 @@ public class AccountService implements UserDetailsService {
             throw new ApplicationException("user-account.validate.username-used");
         }
 
+        // checa se informou a senha
+        if (user.getUnsecurePassword() == null || user.getUnsecurePassword().isEmpty()) {
+            throw new ApplicationException("user-account.validate.invalid-password");
+        }
+        
         // checa se tem permissoes
         if (user.getPermissions().isEmpty()) {
             throw new ApplicationException("user-account.validate.no-permissions");
@@ -153,9 +158,9 @@ public class AccountService implements UserDetailsService {
         // excluimos todas as permissoes atuais
         final Set<Permission> oldPermissions = new HashSet<>(this.permissionRepository.listByUser(user));
 
-        for (Permission permission : oldPermissions) {
+        oldPermissions.stream().forEach((permission) -> {
             this.permissionRepository.delete(permission);
-        }
+        });
 
         // salvamos novamente as permissions
         for (Permission permission : newPermissions) {
