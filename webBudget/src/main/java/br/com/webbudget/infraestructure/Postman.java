541be8dce0559fdc77bca8ece1262723d1f6ec9e
Arthur Gregorio <admarthur.gregorio@pti.org.br>
2015-04-22 13:46:46 -0300

    ref #41, criacao servicos

--- webBudget/src/main/java/br/com/webbudget/infraestructure/Postman.java
+++ webBudget/src/main/java/br/com/webbudget/infraestructure/Postman.java
@@ -16,167 +16,39 @@
  */
 package br.com.webbudget.infraestructure;
 
-import br.com.webbudget.domain.entity.movement.CostCenter;
-import br.com.webbudget.domain.entity.movement.FinancialPeriod;
-import br.com.webbudget.domain.entity.movement.Movement;
-import br.com.webbudget.domain.entity.users.User;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ResourceBundle;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-import javax.servlet.http.HttpServletRequest;
+import br.com.webbudget.domain.mail.MailMessage;
+import br.com.webbudget.domain.service.EmailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 /**
+ * O nosso carteiro, ele envia todas as mensagens de email pela abstracao de 
+ * {@link MailMessage}
  *
  * @author Arthur Gregorio
  *
- * @version 1.0.0
+ * @version 1.1.0
  * @since 1.0.0, 06/07/2014
  */
 @Component
 public class Postman {
 
-//    @Autowired
-//    private MessagesFactory messages;
-//    @Autowired
-//    private JavaMailSender mailSender;
-//    @Autowired
-//    private TemplateEngine templateEngine;
-    
     private final Logger LOG = LoggerFactory.getLogger(Postman.class);
     
     /**
-     * Avisa que um periodo expirou
+     * Metodo utilizado para pegar a mensagem montada e enviar para o destino
      * 
-     * @param financialPeriod 
+     * Este metodo apenas realiza o envio, nenhum processamento extra deve ser
+     * realizado aqui pois a mensagem deve vir montada partindo da implementacao
+     * de uma {@link MailMessage} e passando pelo servico de montagem definido 
+     * em {@link EmailService}
+     * 
+     * @param mailMessage a mensagem a ser enviada
      */
     @Async
-    public void expiredPeriodWarning(FinancialPeriod financialPeriod) {
+    public void sendMail(MailMessage mailMessage) {
         
-    }
-    
-    /**
-     * 
-     * @param movement 
-     */
-    @Async
-    public void movementOverdueWarning(Movement movement) {
-        
-    }
-    
-    /**
-     * 
-     * @param costCenter 
-     */
-    @Async
-    public void costCenterBudgetOverflowWarning(CostCenter costCenter) {
-        
-    }
-    
-    /**
-     * 
-     * @param addressee 
-     */
-    @Async
-    public void newMessageWarning(User addressee) {
-       
-//        LOG.info("Sending new message warning to {}", addressee.getUsername());
-//        
-//        try {
-//            final MimeMessage message = this.mailSender.createMimeMessage();
-//            final MimeMessageHelper helper = this.createHelper(message);
-//
-//            helper.setTo(addressee.getEmail());
-//            helper.setSubject(this.messages.getMessage(
-//                    "mail.private-message.new-message-subject"));
-//        
-//            final Context context = new Context(FacesContext.getCurrentInstance()
-//                    .getViewRoot().getLocale());
-//            
-//            context.setVariable("greeting", this.messages.getMessage(
-//                    "mail.private-message.saudation", addressee.getName()));
-//            context.setVariable("message", this.messages.getMessage("mail.private-message.body", 
-//                    AccountService.getCurrentAuthenticatedUser().getName()));
-//            context.setVariable("urlDescription", this.messages.getMessage("mail.private-message.link"));
-//            context.setVariable("appUrl", this.getApplicationURL().toString());
-//            context.setVariable("logo", this.getLogoPath().toString());
-//            
-//            final String body = this.templateEngine.process("NewMesageWarning.html", context);
-//            
-//            helper.setText(body, true);
-//            
-//            this.mailSender.send(message);
-//                        
-//            LOG.info("New message warning sent to {}", addressee.getUsername());
-//        } catch (MessagingException | MailException ex) {
-//            LOG.error("Error when try to send the message", ex);
-//        }
-    }
-    
-    /**
-     * Encapsula a criacao do helper para poder adicionar o logo da aplicacao 
-     * em cada mensagem
-     * 
-     * @return o helper configurado para a mensagem
-     */
-    private MimeMessageHelper createHelper(MimeMessage message) throws MessagingException {
-       
-        final MimeMessageHelper helper = new MimeMessageHelper(message, true);
-        
-        helper.setFrom(ResourceBundle.getBundle("config/webbudget").getString("mail.user"));
-        
-        return helper;
-    }
-    
-    /**
-     * Metodo que monta o caminho para a logo do sistema
-     * 
-     * @return a url para a logo do sistema
-     */
-    private URL getLogoPath() {
-        
-        final ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-        
-        final String requestDestination = externalContext.getRequestServletPath();
-        final HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
-        
-        final String baseURL = request.getRequestURL().toString().replace(requestDestination, "");
-        
-        try {
-            return new URL(baseURL + "/resources/images/ui-webbudget.png");
-        } catch (MalformedURLException ex) {
-            LOG.error("Erro when try to create logo url", ex);
-            return null;
-        }
-    }
-    
-    /**
-     * Metodo que monta a URL base do sistema
-     * 
-     * @return a url base do sistema
-     */
-    private URL getApplicationURL() {
-        
-        final ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-        
-        final String requestDestination = externalContext.getRequestServletPath();
-        final HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
-        
-        final String baseURL = request.getRequestURL().toString().replace(requestDestination, "");
-        
-        try {
-            return new URL(baseURL);
-        } catch (MalformedURLException ex) {
-            LOG.error("Erro when try to create logo url", ex);
-            return null;
-        }
     }
 }
