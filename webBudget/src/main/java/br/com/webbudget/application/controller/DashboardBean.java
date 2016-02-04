02f0c2a4693f8ccd86e262158a7d69caf5cded89
Arthur Gregorio <arthur.gregorio@pti.org.br>
2016-02-04 14:03:46 -0200

    ref #161

--- webBudget/src/main/java/br/com/webbudget/application/controller/DashboardBean.java
+++ webBudget/src/main/java/br/com/webbudget/application/controller/DashboardBean.java
@@ -17,24 +17,15 @@
 package br.com.webbudget.application.controller;
 
 import br.com.webbudget.application.producer.qualifier.AuthenticatedUser;
-import br.com.webbudget.domain.entity.message.PrivateMessage;
 import br.com.webbudget.domain.entity.movement.FinancialPeriod;
-import br.com.webbudget.domain.entity.movement.Movement;
-import br.com.webbudget.domain.entity.message.UserPrivateMessage;
 import br.com.webbudget.domain.security.User;
-import br.com.webbudget.domain.service.AccountService;
-import br.com.webbudget.domain.service.GraphModelService;
-import br.com.webbudget.domain.service.MovementService;
-import br.com.webbudget.domain.service.PrivateMessageService;
-import java.time.LocalDate;
+import br.com.webbudget.domain.service.FinancialPeriodService;
 import java.util.List;
 import java.util.ResourceBundle;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import lombok.Getter;
-import lombok.Setter;
-import org.primefaces.model.chart.PieChartModel;
 
 /**
  * Mbean utilizado na dashboard do sistema, por ele carregamos os graficos da
@@ -51,33 +42,14 @@ import org.primefaces.model.chart.PieChartModel;
 public class DashboardBean extends AbstractBean {
 
     @Getter
-    @Setter
-    private UserPrivateMessage selectedPrivateMessage;
-
-    @Getter
-    private List<Movement> movements;
-    @Getter
     private List<FinancialPeriod> financialPeriods;
-    @Getter
-    private List<UserPrivateMessage> userPrivateMessages;
-
-    @Getter
-    private PieChartModel revenueModel;
-    @Getter
-    private PieChartModel expensesModel;
 
     @Inject
     @AuthenticatedUser
     private User authenticatedUser;
 
     @Inject
-    private AccountService accountService;
-    @Inject
-    private MovementService movementService;
-    @Inject
-    private GraphModelService graphModelService;
-    @Inject
-    private PrivateMessageService privateMessageService;
+    private FinancialPeriodService financialPeriodService;
 
     /**
      * Inicializa os graficos e tambem carrega as mensagens privadas no box de
@@ -85,73 +57,35 @@ public class DashboardBean extends AbstractBean {
      */
     public void initialize() {
 
-        // carregamos os graficos
-        this.revenueModel = this.graphModelService.buildRevenueModelByCostCenter();
-        this.expensesModel = this.graphModelService.buildExpensesModelByCostCenter();
-
-        // carrega as mensagens do usuario
-        this.userPrivateMessages = this.privateMessageService
-                .listMessagesByUser(this.authenticatedUser, null);
-
-        // carregamos os movimentos para pagamento
-        this.movements = this.movementService
-                .listMovementsByDueDate(LocalDate.now(), true);
+        this.financialPeriods = this.financialPeriodService
+                .listFinancialPeriods(Boolean.FALSE);
     }
 
     /**
-     * Pega mensagem selecionada e mostra a dialog
+     * @return um texto identificando os periodos financeiros em aberto
      */
-    public void displayMessage() {
-        
-        // marca a mensagem como lida
-        this.privateMessageService.markAsRead(this.selectedPrivateMessage);
-        
-        // carrega o nome do cara que enviou
-        final PrivateMessage message = this.selectedPrivateMessage.getPrivateMessage();
-        
-        final User sender = this.accountService.findUserById(message.getSender());
-        
-        message.setSenderName(sender.getName());
-        
-//        this.openDialog("displayPrivateMessageDialog", "dialogDisplayPrivateMessage");
-    }
+    public String getOpenPeriods() {
 
-    /**
-     * Atualiza as mensagens e fecha a popup de mensagem
-     */
-    public void closeMessge() {
-        this.selectedPrivateMessage = null;
-        this.userPrivateMessages = this.privateMessageService
-                .listMessagesByUser(this.authenticatedUser, null);
+        if (this.financialPeriods == null || this.financialPeriods.isEmpty()) {
+            return "";
+        }
 
-//        this.update("messagesList");
-        this.closeDialog("dialogDisplayPrivateMessage");
-    }
+        final StringBuilder builder = new StringBuilder();
 
-    /**
-     * Deleta a mensagem e atualiza a view
-     */
-    public void deleteAndCloseMessage() {
+        for (int i = 0; i < this.financialPeriods.size(); i++) {
 
-        this.privateMessageService.markAsDeleted(this.selectedPrivateMessage);
+            final FinancialPeriod period = this.financialPeriods.get(i);
 
-        this.userPrivateMessages = this.privateMessageService
-                .listMessagesByUser(this.authenticatedUser, null);
+            builder.append(period.getIdentification());
 
-//        this.update("messagesList");
-        this.closeDialog("dialogDisplayPrivateMessage");
-    }
+            if ((i + 1) == (this.financialPeriods.size() - 1)) {
+                builder.append(" e ");
+            } else if (i != (this.financialPeriods.size() - 1)) {
+                builder.append(", ");
+            }
+        }
 
-    /**
-     * Quando este metodo e invocado o sistema realiza um redirecionamento para
-     * a view de pagamento de movimentos para que o movimento selecionado seja
-     * pago pelo usuario
-     *
-     * @param movementId o id do movimento a ser pago
-     * @return a URL para redirecionar o usuario
-     */
-    public String changeToPay(long movementId) {
-        return "financial/movement/formPayment.xhtml?faces-redirect=true&movementId=" + movementId;
+        return builder.toString();
     }
 
     /**
