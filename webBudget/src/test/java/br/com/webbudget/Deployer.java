09b9cae061ee87bbadae84a98aed114dce191aca
arthur <arthur@arthur-pc>
2017-08-21 23:56:44 -0300

    algumas mudancas no pom

--- webBudget/src/test/java/br/com/webbudget/Deployer.java
+++ webBudget/src/test/java/br/com/webbudget/Deployer.java
@@ -14,59 +14,59 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package br.com.webbudget;
-
-import br.com.webbudget.domain.model.entity.converter.JPALocalDateConverter;
-import br.com.webbudget.domain.model.entity.converter.JPALocalTimeConverter;
-import br.com.webbudget.application.producer.EntityManagerProducer;
-import br.com.webbudget.application.producer.LoggerProducer;
-import br.com.webbudget.infraestructure.configuration.ApplicationUtils;
-import java.io.File;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.shrinkwrap.resolver.api.maven.Maven;
-
-/**
- * Classe helper para realizar o deploy do war de testes
- *
- * @author Arthur Gregorio
- *
- * @version 1.0.0
- * @since 2.3.1, 24/10/2016
- */
-public class Deployer {
-
-    /**
-     * Executa o deploy da infra de testes
-     * 
-     * @return o archive para deploy
-     */
-    public static WebArchive deploy() {
-        
-        // libs do projeto
-        final File[] libraries = Maven.resolver()
-                .loadPomFromFile("pom.xml")
-                .importCompileAndRuntimeDependencies()
-                .resolve()
-                    .withTransitivity()
-                    .asFile();
-        
-        // cria o arquivo war
-        return ShrinkWrap.create(WebArchive.class, "wb-test.war")
-                .addPackages(true, 
-                        "br.com.webbudget.domain",
-                        "br.com.webbudget.application.component.table")
-                .addClasses(
-                        JPALocalDateConverter.class,
-                        JPALocalTimeConverter.class,
-                        LoggerProducer.class,
-                        ApplicationUtils.class,
-                        EntityManagerProducer.class)
-                .addAsResource("META-INF/persistence.xml")
-                .addAsResource(new File("src/main/resources/webbudget.properties"))
-                .addAsWebInfResource("test-jboss-deployment-structure.xml", "jboss-deployment-structure.xml")
-                .addAsWebInfResource(new File("src/main/webapp/WEB-INF/beans.xml"))
-                .addAsLibraries(libraries);
-    }
-}
+//package br.com.webbudget;
+//
+//import br.com.webbudget.domain.model.entity.converter.JPALocalDateConverter;
+//import br.com.webbudget.domain.model.entity.converter.JPALocalTimeConverter;
+//import br.com.webbudget.application.producer.EntityManagerProducer;
+//import br.com.webbudget.application.producer.LoggerProducer;
+//import br.com.webbudget.infraestructure.configuration.ApplicationUtils;
+//import java.io.File;
+//import org.jboss.shrinkwrap.api.ShrinkWrap;
+//import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+//import org.jboss.shrinkwrap.api.spec.WebArchive;
+//import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+//
+///**
+// * Classe helper para realizar o deploy do war de testes
+// *
+// * @author Arthur Gregorio
+// *
+// * @version 1.0.0
+// * @since 2.3.1, 24/10/2016
+// */
+//public class Deployer {
+//
+//    /**
+//     * Executa o deploy da infra de testes
+//     * 
+//     * @return o archive para deploy
+//     */
+//    public static WebArchive deploy() {
+//        
+//        // libs do projeto
+//        final File[] libraries = Maven.resolver()
+//                .loadPomFromFile("pom.xml")
+//                .importCompileAndRuntimeDependencies()
+//                .resolve()
+//                    .withTransitivity()
+//                    .asFile();
+//        
+//        // cria o arquivo war
+//        return ShrinkWrap.create(WebArchive.class, "wb-test.war")
+//                .addPackages(true, 
+//                        "br.com.webbudget.domain",
+//                        "br.com.webbudget.application.component.table")
+//                .addClasses(
+//                        JPALocalDateConverter.class,
+//                        JPALocalTimeConverter.class,
+//                        LoggerProducer.class,
+//                        ApplicationUtils.class,
+//                        EntityManagerProducer.class)
+//                .addAsResource("META-INF/persistence.xml")
+//                .addAsResource(new File("src/main/resources/webbudget.properties"))
+//                .addAsWebInfResource("test-jboss-deployment-structure.xml", "jboss-deployment-structure.xml")
+//                .addAsWebInfResource(new File("src/main/webapp/WEB-INF/beans.xml"))
+//                .addAsLibraries(libraries);
+//    }
+//}
--- webBudget/src/test/java/br/com/webbudget/entries/CardTest.java
+++ webBudget/src/test/java/br/com/webbudget/entries/CardTest.java
@@ -14,86 +14,86 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package br.com.webbudget.entries;
-
-import br.com.webbudget.Deployer;
-import br.com.webbudget.domain.model.entity.entries.Card;
-import br.com.webbudget.domain.model.entity.entries.CardType;
-import br.com.webbudget.domain.model.service.CardService;
-import java.math.BigDecimal;
-import javax.inject.Inject;
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.testng.Arquillian;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-/**
- * Teste unitario para validacao da parte de cartoes 
- * 
- * @author Arthur Gregorio
- *
- * @version 1.0.0
- * @since 2.3.1, 24/11/2016
- */
-public class CardTest extends Arquillian {
-
-    @Inject
-    private CardService cardService;
-    
-    /**
-     * @return o wararchive para deploy do teste
-     */
-    @Deployment
-    public static WebArchive deploy() {
-        return Deployer.deploy();
-    }
-    
-    /**
-     * Valida a insercao do objeto
-     */
-    @Test(priority = 1)
-    public void insertCard() {
-
-        final Card card = new Card();
-        
-        card.setCardType(CardType.CREDIT);
-        card.setCreditLimit(new BigDecimal(200));
-        card.setExpirationDay(10);
-        card.setFlag("VISA");
-        card.setName("Cartao Teste");
-        card.setNumber("1234567890");
-        card.setOwner("Jovem Brasileiro");
-        
-        this.cardService.saveCard(card);
-        
-        final Card saved = this.cardService
-                .findCardByNumberAndType("1234567890", CardType.CREDIT);
-        
-        Assert.assertNotNull(saved, "Test card not found");
-    }
-    
-    /**
-     * 
-     */
-    @Test(priority = 2)
-    public void listCards() {
-
-    }
-    
-    /**
-     * 
-     */
-    @Test(priority = 3)
-    public void updateCard() {
-
-    }
-    
-    /**
-     * 
-     */
-    @Test(priority = 4)
-    public void deleteCard() {
-
-    }
-}
+//package br.com.webbudget.entries;
+//
+//import br.com.webbudget.Deployer;
+//import br.com.webbudget.domain.model.entity.entries.Card;
+//import br.com.webbudget.domain.model.entity.entries.CardType;
+//import br.com.webbudget.domain.model.service.CardService;
+//import java.math.BigDecimal;
+//import javax.inject.Inject;
+//import org.jboss.arquillian.container.test.api.Deployment;
+//import org.jboss.arquillian.testng.Arquillian;
+//import org.jboss.shrinkwrap.api.spec.WebArchive;
+//import org.testng.Assert;
+//import org.testng.annotations.Test;
+//
+///**
+// * Teste unitario para validacao da parte de cartoes 
+// * 
+// * @author Arthur Gregorio
+// *
+// * @version 1.0.0
+// * @since 2.3.1, 24/11/2016
+// */
+//public class CardTest extends Arquillian {
+//
+//    @Inject
+//    private CardService cardService;
+//    
+//    /**
+//     * @return o wararchive para deploy do teste
+//     */
+//    @Deployment
+//    public static WebArchive deploy() {
+//        return Deployer.deploy();
+//    }
+//    
+//    /**
+//     * Valida a insercao do objeto
+//     */
+//    @Test(priority = 1)
+//    public void insertCard() {
+//
+//        final Card card = new Card();
+//        
+//        card.setCardType(CardType.CREDIT);
+//        card.setCreditLimit(new BigDecimal(200));
+//        card.setExpirationDay(10);
+//        card.setFlag("VISA");
+//        card.setName("Cartao Teste");
+//        card.setNumber("1234567890");
+//        card.setOwner("Jovem Brasileiro");
+//        
+//        this.cardService.saveCard(card);
+//        
+//        final Card saved = this.cardService
+//                .findCardByNumberAndType("1234567890", CardType.CREDIT);
+//        
+//        Assert.assertNotNull(saved, "Test card not found");
+//    }
+//    
+//    /**
+//     * 
+//     */
+//    @Test(priority = 2)
+//    public void listCards() {
+//
+//    }
+//    
+//    /**
+//     * 
+//     */
+//    @Test(priority = 3)
+//    public void updateCard() {
+//
+//    }
+//    
+//    /**
+//     * 
+//     */
+//    @Test(priority = 4)
+//    public void deleteCard() {
+//
+//    }
+//}
