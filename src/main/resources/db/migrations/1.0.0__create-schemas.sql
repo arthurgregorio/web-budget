363eb585f6788d0b5d73e585042c4a347c7722e3
Arthur Gregório <arthurshakal@gmail.com>
2018-11-04 23:44:39 -0200

    add default migrations and new RC profile

---
+++ src/main/resources/db/migrations/1.0.0__create-schemas.sql
@@ -0,0 +1,12 @@
+-- create the database schemas
+CREATE SCHEMA financial;
+CREATE SCHEMA financial_audit;
+
+CREATE SCHEMA journal;
+CREATE SCHEMA journal_audit;
+
+CREATE SCHEMA registration;
+CREATE SCHEMA registration_audit;
+
+CREATE SCHEMA tools;
+CREATE SCHEMA tools_audit;
