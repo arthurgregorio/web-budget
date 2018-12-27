7d3b5a3ea19c46300051ba75646c5a3f611b4f7d
Arthur Gregório <arthurshakal@gmail.com>
2018-12-26 22:13:26 -0200

    ref #336

--- src/main/java/br/com/webbudget/infrastructure/jpa/CustomNamingStrategy.java
+++ src/main/java/br/com/webbudget/infrastructure/jpa/CustomNamingStrategy.java
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package br.com.webbudget.infrastructure.jpa;
 
 import org.hibernate.boot.model.naming.Identifier;
