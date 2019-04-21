aa91ee90ad20b6111cd6f3e41388ec94b91d588d
Arthur Gregório <arthurshakal@gmail.com>
2019-04-21 01:21:54 -0300

    ref #362

--- src/main/webapp/resources/webbudget/js/chart-manager.js
+++ src/main/webapp/resources/webbudget/js/chart-manager.js
@@ -39,9 +39,34 @@ function drawLineChart(data, canvas) {
         type: 'line',
         data: data,
         options: {
-            responsive: true
+            responsive: true,
+            tooltips: {
+                callbacks: {
+                    label: function (tooltipItem, data) {
+                        return " " + formatAsMonetaryValue(tooltipItem.value);
+                    }
+                }
+            }
         }
     };
 
     new Chart(document.getElementById(canvas).getContext('2d'), configuration);
+}
+
+/**
+ * Simple function to format the value as monetary value
+ *
+ * @param number to be formatted
+ * @returns {string} the formatted number
+ */
+function formatAsMonetaryValue(number) {
+    number += '';
+    x = number.split('.');
+    x1 = x[0];
+    x2 = x.length > 1 ? ',' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + '.' + '$2');
+    }
+    return "R$ " + x1 + x2;
 }
