a2a056ce8d1a05017ede31b1aadf75b00419c1f1
Arthur Gregório <arthurshakal@gmail.com>
2019-08-28 00:27:50 -0300

    ref #451

--- src/main/java/br/com/webbudget/application/controller/RecoverPasswordBean.java
+++ src/main/java/br/com/webbudget/application/controller/RecoverPasswordBean.java
@@ -49,7 +49,7 @@ public class RecoverPasswordBean extends AbstractBean {
      */
     public void recoverPassword() {
 
-//        this.recoverPasswordService.recover(this.email);
+        this.recoverPasswordService.recover(this.email);
 
         this.closeDialog("dialogRecoverPassword");
         this.addInfoAndKeep("recover-password.email-sent");
